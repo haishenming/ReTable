671b999b3e0060735036380804f9109677a6bf7b
xiangyufan <xiangyufan@juwang.cn>
2017-08-16 17:25:16 +0800

    debug

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ DB_CONFIG = {
     'host': '101.201.68.4',  # 数据库地址
     'port': 3306,  # 数据库端口
     'user': 'root',  # 用户名
-    'password': 'haishenming123',  # 密码
-    'database': 'jc_backstage2',  # 数据库
+    'password': '',  # 密码
+    'database': '',  # 数据库
 }
 
 # 开发数据
