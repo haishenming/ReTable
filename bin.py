55a3898e07980e5b01758a13901c01eb8f05c219
xiangyufan <xiangyufan@juwang.cn>
2017-08-18 18:05:41 +0800

    优化获取数据库配置流程

--- bin.py
+++ bin.py
@@ -5,14 +5,15 @@ from config import *
 
 
 def handle(pars):
-    environments = {
-        'dev': DB_DEV,
-        'test': DB_TEST,
-        'conf': DB_CONFIG,
-    }
+    # 获取配置文件数据
+    envs = DB_CONFIG()
+    if hasattr(envs, pars.environment):
+        env = getattr(envs, pars.environment)
+    else:
+        env = {}
+        print("环境未配置！")
 
     op = pars.operate
-    env = environments[pars.environment]
     tables_name = pars.tables.split(",")
     env.update({"tables_name": tables_name})
     table = Table(**env)
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--operate',
                         choices=['write', 'change_field_name', 'change_table_name', 'add_field', 'del_field'],
                         help='写入/修改字段/修改表名/添加字段')
-    parser.add_argument('-e', '--environment', default='conf', help='数据库环境')
+    parser.add_argument('-e', '--environment', default='tt_conf', help='数据库环境')
     parser.add_argument('-t', '--tables', default='', help='要操作的表名')
 
     args = parser.parse_args()
--- config.py
+++ config.py
@@ -1,23 +1,26 @@
 import os
 
-# 数据库配置
-DB_CONFIG = {
-    'host': '101.201.68.4',  # 数据库地址
-    'port': 3306,  # 数据库端口
-    'user': 'root',  # 用户名
-    'password': '',  # 密码
-    'database': '',  # 数据库
-}
 
-# 开发数据
-DB_DEV = {
 
-}
+class DB_CONFIG:
+    # 数据库配置
+    tt_conf = {
+        'host': '101.201.68.4',  # 数据库地址
+        'port': 3306,  # 数据库端口
+        'user': 'root',  # 用户名
+        'password': '',  # 密码
+        'database': '',  # 数据库
+    }
 
-# 测试数据库
-DB_TEST = {
+    # 开发数据
+    dev = {
 
-}
+    }
+
+    # 测试数据库
+    test = {
+
+    }
 
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
